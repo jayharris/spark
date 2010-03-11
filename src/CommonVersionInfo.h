34908a3cad466367980b4ac8217d2f17132f8e40
unknown <HarrisJa@.val.vlss.local>
2010-03-11 14:20:33 -0500

    Build against ASP.NET MVC 2 RTM

--- src/CommonVersionInfo.h
+++ src/CommonVersionInfo.h
@@ -4,6 +4,6 @@
 #define VERSIONINFO_MINOR 0
 #define VERSIONINFO_BUILD 1
 #define VERSIONINFO_REVISION 0
-#define VERSIONINFO_COPYRIGHT "Copyright (c) Louis DeJardin 2008-2009"
+#define VERSIONINFO_COPYRIGHT "Copyright (c) Louis DeJardin 2008-2010"
 #define VERSIONINFO_COMPANY "Louis DeJardin"
 #define VERSIONINFO_PRODUCT "Spark"
